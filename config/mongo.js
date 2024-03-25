bc1e5faf9fe61d40e5ccb66681b38dc55a87dfa0
naufalsoerya <naufalsurya.official@gmail.com>
2024-03-25 15:21:03 +0700

    updated: first setup done

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://naufalsoerya:r6L30uaXRxG7IXsU@gc01-p3.utp1yek.mongodb.net/?retryWrites=true&w=majority&appName=GC01-P3";
+const uri = process.env.MONGO_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
